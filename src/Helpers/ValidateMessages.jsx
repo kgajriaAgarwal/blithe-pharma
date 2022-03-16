f489404428299e0975ab46cf0884ebf74c92f5cf
Karishma Gajria <gajriakarishma123@gmail.com>
2022-03-16 14:22:38 +0530

    authentication feature , forgot password and routing implemented

---
+++ src/Helpers/ValidateMessages.jsx
@@ -0,0 +1,23 @@
+const validationMessages = {
+    firstName:{
+        required: 'Please enter first name'
+    },
+    lastName:{
+        required: 'Please enter last name'
+    },
+    password:{
+        required: 'Please enter password'
+    },
+    confirmPassword:{
+        required: 'Please enter confirm password'
+    },
+    email:{
+        required: 'Please enter email',
+        invalid: 'please enter a valid email address'
+    },
+    fieldRequired:{
+        required: 'This field is required'
+    }
+} 
+
+export default validationMessages;
---
+++ src/Pages/Authentication/Login/Login.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import '../Css/Authentication.css';
+import Input from '../../../Shared/Input/Input';
+import blitheCollage from '../../../Assets/Images/Blithe-collage.jpg';
+import blitheLogo from '../../../Assets/Images/Blithe-logo.png';
+import InputField from '../../../Shared/Input/InputField';
+
+const Login = () =>{
+
+    const [showPassword , setShowPassword ] = useState(false);
+    const [loginData, setLoginData] = useState(
+        {   
+            email:'',
+            password:''  
+        })
+
+    //Handle show/hide password
+    const handlePassword = () =>{
+        setShowPassword(!showPassword)
+    }
+
+    const handleLogin = (e) =>{
+        e.preventDefault();
+        //commented right now for future perspective..
+        // console.log('LoginData', loginData);
+    }
+
+    return(
+        <>
+            <section className="auth">
+            <div className="col-7 auth-img-container">
+                    <img src={blitheCollage}
+                        alt="auth-img" />
+                </div> 
+                <div className="col-7 auth-content-container">
+                    <div className="auth-form">
+                        <img src={blitheLogo} className="blithe-logo" alt="blithe-logo"/>
+                        <form>
+                            <p className="heading-md heading">Login</p>
+                            <InputField 
+                                type='email'
+                                title='Email'
+                                name='email'
+                                placeholder='Enter email'
+                                value={loginData?.email? loginData.email : ''}
+                                onChange={(e)=>setLoginData(prevData => {return  {...prevData, email:e.target.value}})}
+                            />
+                             <InputField 
+                                type={showPassword?'text':'password'}
+                                title='Password'
+                                name='password'
+                                placeholder='Enter password'
+                                showPassword={showPassword}
+                                handlePassword = {handlePassword}
+                                value={loginData?.password? loginData.password : ''}
+                                onChange={(e)=>setLoginData(prevData => {return  {...prevData, password:e.target.value}})}
+                            />
+
+                            <div className="remember">
+                                <label>
+                                    <Input id='rememberUser' type="checkbox" name ='remember' checked={true}/>
+                                    Remember me!
+                                </label>
+                                <br/>
+                                <Link  to="/forgot-password" className="btn-link-forgot-password" target="_blank">Forgot Password ??</Link>
+                            </div>
+                            <button className="btn btn-sm btn-primary" type="submit" onClick={handleLogin}>Sign In</button>
+                            <div className="inputBx">
+                                {/* <p className="text-md">Don't have an account?<a className="btn text-decoration-none btn-sm btn-link"
+                                        href="../SignUp/SignUp.html">Sign Up</a></p> */}
+                                <p className="text-md">Don't have an account?
+                                    <Link  to="/signup" className="btn text-decoration-none btn-sm btn-link">Sign Up</Link>
+                                </p>
+                            </div>
+                        </form> 
+                        <div className="sm-container">
+                            <p className="text-lg">Login with social media:</p>
+                            <div className="flex-content">
+                                <img src="https://img.icons8.com/color/48/000000/facebook-circled--v1.png" className="icon-btn-img"
+                                    alt="icon-btn-img" />
+                                <img src="https://img.icons8.com/nolan/48/instagram-new.png" className="icon-btn-img"
+                                    alt="icon-btn-img" />
+                                <img src="https://img.icons8.com/fluency/48/000000/twitter.png" className="icon-btn-img"
+                                    alt="icon-btn-img" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Login;
